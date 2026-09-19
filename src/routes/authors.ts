5a51b21eaf78d7907eb8d83b999dbcdadb2725d3
Khaviso Vukeya <223881260@edu.vut.ac.za>
2026-09-20 00:43:30 +0200

    Created fuction to get author by ID

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -4,3 +4,13 @@ import { authors, books } from '../data/store';
 import { validateAuthor } from '../middleware/validate';
 import { NotFoundError, ConflictError } from '../utils/errors';
 import { Author } from '../models/Author';
+
+const router = Router();
+
+function getAuthorById(id: string): Author {
+    const author = authors.find((a) => a.id === id);
+    if (!author) {
+        throw new NotFoundError(`Author with id "${id}" not found`);
+    }
+    return author
+}
