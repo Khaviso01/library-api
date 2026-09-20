ae7b548b0033b8af5baa71ef9df6de1b67a1d0dd
Khaviso Vukeya <223881260@edu.vut.ac.za>
2026-09-20 15:00:30 +0200

    Created in-memory data store for authors and books, and migrated the code

--- src/middleware/errorHandler.ts
+++
@@ -1,6 +0,0 @@
-import { AUthor } from '../models/Author'
-import { Book } from '../models/Book'
-
-// in-memory database, resets when server restarts
-export const authors: Author[] = [];
-export const books: Book[] = [];
